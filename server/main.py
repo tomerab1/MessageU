0890904080c23d8e8399b0dbe64315655f7d3c02
Tomer <tomer1452002@gmail.com>
2025-02-25 19:00:27 +0200

    added header and payload classes in request

--- server/main.py
+++ server/main.py
@@ -1,10 +1,27 @@
 from config.config import Config
+import struct
+from proto.request import Request
 
 
 def main():
     Config.load()
 
-    print(Config.PORT)
+    print(f"port: {Config.PORT}")
+
+    client_id = b"1234567890123456"
+    version = 1
+    code = 1000
+    payload = b"Hello, world!"
+    payload_size = len(payload)
+
+    packet = (
+        struct.pack(Request._HEADER_FMT, client_id, version, code, payload_size)
+        + payload
+    )
+
+    req = Request(packet)
+    print("Header:", req.get_header())
+    print("Payload:", req.get_payload())
 
 
 if __name__ == "__main__":
--- server/proto/request.py
+++ server/proto/request.py
@@ -1,9 +1,32 @@
 import struct
+from dataclasses import dataclass
 
 
 class Request:
-    _HEADER_FMT = "!16sBHI"
+    _HEADER_FMT = "<16sBHI"
     _HEADER_SZ = struct.calcsize(_HEADER_FMT)
 
+    @dataclass
+    class Header:
+        client_id: str
+        version: int
+        code: int
+
+    @dataclass
+    class Payload:
+        payload: bytes
+
     def __init__(self, packet):
-        self._raw_data = struct.unpack(Request._HEADER_FMT, packet)
+        data = struct.unpack(Request._HEADER_FMT, packet[: Request._HEADER_SZ])
+        client_id, version, code, payload_size = data
+
+        self._header = Request.Header(client_id, version, code)
+        self._payload = Request.Payload(
+            packet[Request._HEADER_SZ : Request._HEADER_SZ + payload_size]
+        )
+
+    def get_header(self):
+        return self._header
+
+    def get_payload(self):
+        return self._payload
